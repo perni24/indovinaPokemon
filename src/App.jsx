cde7da101df9598d67a4c869be2d4544a0c79108
perni24 <perni24lb@gmail.com>
2024-09-03 23:22:03 +0200

    modifiche 03/09/2024

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className='app'>
         <div className='cerchio'>
           <div className='cerchio2'>
-          {gameState === 1 ? <StartPage gameState={gameState} setGameState={setGameState}/>: gameState === 2 ? <GamePage gameState={gameState} setGameState={setGameState}/> : <EndPage/>}
+            {gameState === 1 ? <StartPage gameState={gameState} setGameState={setGameState}/>: gameState === 2 ? <GamePage gameState={gameState} setGameState={setGameState}/> : <EndPage/>}
           </div>
         </div>
       </div>
